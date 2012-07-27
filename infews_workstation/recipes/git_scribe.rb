a1213d281f5850d052a3fd9eeb5d9902af5be91d
Davis W. Frank <dwfrank@infe.ws>
2012-07-27 08:28:06 -0700

    deps for git-scribe for writing

---
+++ infews_workstation/recipes/git_scribe.rb
@@ -0,0 +1,7 @@
+#
+# Dependencies for Git Scribe: https://github.com/schacon/git-scribe
+
+brew 'asciidoc'
+brew 'source-highlight'
+brew 'fop'
+brew 'https://raw.github.com/adamv/homebrew-alt/master/non-free/kindlegen.rb'
