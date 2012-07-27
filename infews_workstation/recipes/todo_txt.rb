47bba637a0cd20ee647b3202cdfa3900efaf498a
Davis W. Frank <dwfrank@infe.ws>
2012-07-27 08:29:56 -0700

    use WS_USER

--- infews_workstation/recipes/todo_txt.rb
+++ infews_workstation/recipes/todo_txt.rb
@@ -1,7 +1,19 @@
+# Custom todo.txt install
+#
+# Dependencies
+# * Homebrew
+# * Pivotal Workstation Brew provider
+# * Dropbox being installed
+# * ~/Dropbox/todo/config being present and having the user's configuration
+#
+#
+# Copyright 2012 Infews LLC
+# Licensed via the MIT license
+
 brew "todo-txt"
 
-directory "/Users/#{node[:username]}/.todo" do
-  owner node[:username]
+directory "/Users/#{WS_USER}/.todo" do
+  owner WS_USER
 end
 
 execute "copy config file from Dropbox" do
