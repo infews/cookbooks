b5b73ed6db55034a2fdd74b995f828fad2af6a14
Davis W. Frank <dwfrank@infe.ws>
2012-07-28 23:41:13 -0700

    type fixes; attempt at scale affect

--- infews_workstation/recipes/os_x_defaults_dock.rb
+++ infews_workstation/recipes/os_x_defaults_dock.rb
@@ -5,13 +5,13 @@
 pivotal_workstation_defaults "Show indicator lights for open applications in the Dock" do
   domain 'com.apple.dock'
   key 'show-process-indicators'
-  boolean 'true'
+  boolean true
 end
 
 pivotal_workstation_defaults "Enable the 2D Dock" do
   domain 'com.apple.dock'
   key 'no-glass'
-  boolean 'true'
+  boolean true
 end
 
 pivotal_workstation_defaults "Position the Dock on the left" do
@@ -23,20 +23,26 @@ end
 pivotal_workstation_defaults "Automatically hide and show the Dock" do
   domain 'com.apple.dock'
   key 'autohide'
-  boolean 'true'
+  boolean true
+end
+
+pivotal_workstation_defaults "Minimized with scaled effect" do
+  domain 'com.apple.dock'
+  key 'mineffect'
+  string 'scale'
 end
 
 pivotal_workstation_defaults "Make Dock icons of hidden applications translucent" do
   domain 'com.apple.dock'
   key 'showhidden'
-  boolean 'true'
+  boolean true
 end
 
 pivotal_workstation_defaults "Enable iTunes track notifications in the Dock" do
   domain 'com.apple.dock'
   key 'itunes-notifications'
-  boolean 'true'
+  boolean true
 end
 
 ## Reset Launchpad
-execute 'find ~/Library/ Application \ Support/Dock -name "*.db" -maxdepth 1 -delete'
+execute "find '/Users/#{WS_USER}/Library/Application Support/Dock' -name *.db -maxdepth 1 -delete"
