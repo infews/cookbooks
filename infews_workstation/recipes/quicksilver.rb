61b7597316ee2b29e0a44b6146833e09b4c86ee4
Davis W. Frank <dwfrank@infe.ws>
2012-07-29 00:02:55 -0700

    works. see comment in file.

--- infews_workstation/recipes/quicksilver.rb
+++ infews_workstation/recipes/quicksilver.rb
@@ -1,4 +1,22 @@
-pivotal_workstation_package "Quicksilver" do
-  source "https://github.com/downloads/quicksilver/Quicksilver/Quicksilver%20#{node[:qs_version]}.dmg"
+
+# This *should* work, but there's some chef or package provider bug w/ spaces in URLs and/or DMG names
+#
+#pivotal_workstation_package "Quicksilver" do
+#  app "Quicksilver"
+#  source "https://github.com/downloads/quicksilver/Quicksilver/Quicksilver%20#{node[:qs_version]}.dmg'
+#  dmg_name "Quicksilver\ #{node[:qs_version]}"
+#  action :install
+#end
+
+# So instead we download the DMG in the shell and then give the package provider what it wants
+execute "Download Quicksilver" do
+  command "curl -L https://github.com/downloads/quicksilver/Quicksilver/Quicksilver%20#{node[:qs_version]}.dmg -o /var/chef/cache/quicksilver.dmg"
+end
+
+pivotal_workstation_package "Quicksilver install" do
+  app "Quicksilver"
+  dmg_name "quicksilver"
   action :install
 end
+
+
