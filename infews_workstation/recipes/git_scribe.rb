f2ef8c7548a9e224aa493bb2077a6bcaf1e6d608
Davis W. Frank <dwfrank@.infe.ws>
2012-08-05 10:09:11 -0700

    Fix git scribe recipe for latest Kindlegen

--- infews_workstation/recipes/git_scribe.rb
+++ infews_workstation/recipes/git_scribe.rb
@@ -6,4 +6,7 @@ brew 'source-highlight'
 brew 'fop'
 
 # This appears to point to a missing file
-#brew 'https://raw.github.com/adamv/homebrew-alt/master/non-free/kindlegen.rb'
+brew 'https://raw.github.com/infews/homebrew-alt/master/non-free/kindlegen.rb'
+
+# I don't know enough about where the error is, but the Kindlegen binary isn't executable
+execute "chmod +x /usr/local/Cellar/kindlegen/2.5/bin/kindlegen"
