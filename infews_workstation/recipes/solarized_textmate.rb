f4df3f321cdc933f8f34b8daf427d5d6b361df3d
Davis W. Frank <dwfrank@.infe.ws>
2012-08-06 08:28:42 -0700

    Fix to actually copy the Solarized themes into TextMate

--- infews_workstation/recipes/solarized_textmate.rb
+++ infews_workstation/recipes/solarized_textmate.rb
@@ -6,19 +6,14 @@ execute "Clone Textmate/Solarized" do
   not_if { File.exist? "#{solarized_dir}" }
 end
 
-textmate_themes_directory = %Q{#{WS_HOME}/Library/Application\ Support/TextMate/Themes}
+textmate_themes_directory = %Q{#{WS_HOME}/Library/Application\ Support/TextMate/Themes/}
 
 directory textmate_themes_directory do
   user WS_USER
   action :create
 end
 
-execute "Add Solarized Dark for TextMate" do
-  command "cp \"#{solarized_dir}/Solarized\ (dark).tmTheme\" #{textmate_themes_directory}"
-  user WS_USER
-end
-
-execute "Add Solarized Light for TextMate" do
-  command "cp \"#{solarized_dir}/Solarized\ (light).tmTheme\" #{textmate_themes_directory}"
+execute "Add Solarized themes to TextMate" do
+  command %Q{cp #{solarized_dir}/*.tmTheme "#{textmate_themes_directory}"}
   user WS_USER
 end
